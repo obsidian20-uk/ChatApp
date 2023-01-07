e7cf088063b31b411cf89739a7f11d41a20d2826
Paul Simmons <paul@simmonsonline.me.uk>
2023-01-07 10:17:08 +0000

    Add project files.

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public const string HubUrl = "/chat";
+
+        public async Task Broadcast(string username, string message)
+        {
+            await Clients.All.SendAsync("Broadcast", username, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Debug.WriteLine($"{Context.ConnectionId} connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            Debug.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(e);
+        }
+    }
+}
---
+++ ChatApp/Pages/Index.razor.cs
@@ -0,0 +1,102 @@
+﻿using ChatApp.Hubs;
+using ChatApp.Models;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ChatApp.Pages
+{
+    public partial class Index
+    {
+        // flag to indicate chat status
+        private bool _isChatting = false;
+
+        // name of the user who will be chatting
+        private string _username;
+
+        // on-screen message
+        private string _message;
+
+        // new message input
+        private string _newMessage;
+
+        // list of messages in chat
+        private List<Message> _messages = new List<Message>();
+
+        private string _hubUrl;
+        private HubConnection _hubConnection;
+
+        public async Task Chat()
+        {
+            // check username is valid
+            if (string.IsNullOrWhiteSpace(_username))
+            {
+                _message = "Please enter a name";
+                return;
+            };
+
+            try
+            {
+                // Start chatting and force refresh UI.
+                _isChatting = true;
+                await Task.Delay(1);
+
+                // remove old messages if any
+                _messages.Clear();
+
+                // Create the chat client
+                string baseUrl = navigationManager.BaseUri;
+
+                _hubUrl = baseUrl.TrimEnd('/') + ChatHub.HubUrl;
+
+                _hubConnection = new HubConnectionBuilder()
+                    .WithUrl(_hubUrl)
+                    .Build();
+
+                _hubConnection.On<string, string>("Broadcast", BroadcastMessage);
+
+                await _hubConnection.StartAsync();
+
+                await SendAsync($"[Notice] {_username} joined chat room.");
+            }
+            catch (Exception e)
+            {
+                _message = $"ERROR: Failed to start chat client: {e.Message}";
+                _isChatting = false;
+            }
+        }
+
+        private void BroadcastMessage(string name, string message)
+        {
+            bool isMine = name.Equals(_username, StringComparison.OrdinalIgnoreCase);
+
+            _messages.Add(new Message(name, message, isMine));
+
+            // Inform blazor the UI needs updating
+            InvokeAsync(StateHasChanged);
+        }
+
+        private async Task DisconnectAsync()
+        {
+            if (_isChatting)
+            {
+                await SendAsync($"[Notice] {_username} left chat room.");
+
+                await _hubConnection.StopAsync();
+                await _hubConnection.DisposeAsync();
+
+                _hubConnection = null;
+                _isChatting = false;
+            }
+        }
+
+        private async Task SendAsync(string message)
+        {
+            if (_isChatting && !string.IsNullOrWhiteSpace(message))
+            {
+                await _hubConnection.SendAsync("Broadcast", _username, message);
+
+                _newMessage = string.Empty;
+            }
+        }
+    }
+}
